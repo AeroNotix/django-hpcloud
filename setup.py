d5a5c03f2381e697d1fee33622214541388aecd9
Aaron <aaron.l.france@gmail.com>
2013-03-13 00:27:32 +0100

    Added the setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='django_hpcloud',
+      version='0.1',
+      description='HPCloud module for Django',
+      author='Aaron France',
+      author_email='aaron.france@hp.com',
+      url='http://github.com/AeroNotix/django-hpcloud',
+      package_dir={'django_hpcloud': ''},
+      packages=['django_hpcloud'],
+     )
