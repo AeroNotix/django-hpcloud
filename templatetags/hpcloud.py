867d40f82ff66eca816a85d31abc5ed349919600
Aaron <aaron.l.france@gmail.com>
2013-03-12 21:16:44 +0100

    Finished up the template tag for the CDN url generator.

--- templatetags/hpcloud.py
+++ templatetags/hpcloud.py
@@ -10,10 +10,10 @@ class CDNUrl(template.Node):
         self.container = container
         self.path = path
         self.protocol = protocol
-    def render(self):
-        cdn(self.container, self.path, self.protocol)
+    def render(self, context):
+        return cdn(self.container, self.path, self.protocol)
 
-def cdn(container, path, protocol="http"):
+def cdn(container, path, protocol):
     cdncontainer = CDN_ENABLED_CONTAINERS.get(container)
     if cdncontainer:
         return "%s/%s" % (cdncontainer[protocol], path)
@@ -21,6 +21,10 @@ def cdn(container, path, protocol="http"):
         return ""
 
 def parse_cdn(parser, token):
-    tokens = token.split_tokens()
+    tokens = token.split_contents()
+    if len(tokens) == 3:
+        return CDNUrl(tokens[1], tokens[2], "http")
     if len(tokens) == 4:
-        raise NOTFUCKINGIMPLEMENTEDERROR
+        return CDNUrl(tokens[1], tokens[2], tokens[3])
+
+register.tag('cdn', parse_cdn)
