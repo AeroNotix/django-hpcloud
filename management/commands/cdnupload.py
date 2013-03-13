34f9597bc97a67770413bbd840916373f291790e
AeroNotix <aaron.l.france@gmail.com>
2013-03-13 13:10:40 +0100

    Reworked the methods into the main class to share data.

--- management/commands/cdnupload.py
+++ management/commands/cdnupload.py
@@ -1,6 +1,7 @@
 import os, sys
-from stat import *
 import urllib2
+import mimetypes; mimetypes.init()
+from stat import *
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -11,43 +12,54 @@ class Command(BaseCommand):
     help = 'Uploads all static content into the HPCloud CDN'
 
     def handle(self, *args, **options):
+        try:
+            self.settings = settings.HPCLOUD_TTL_MAP
+        except AttributeError:
+            self.settings = {}
+
         for directory in settings.STATICFILES_DIRS:
-            process_directory(directory, directory)
+            self.basedir = directory
+            self.process_directory(directory)
 
-def process_directory(basedir, top):
-    for f in os.listdir(top):
-        pathname = os.path.join(top, f)
-        mode = os.stat(pathname).st_mode
-        if S_ISDIR(mode):
-            create_directory(basedir, top, pathname)
-            process_directory(basedir, pathname)
-        elif S_ISREG(mode):
-            upload_file(basedir, top, pathname)
+    def process_directory(self, top):
+        for f in os.listdir(top):
+            pathname = os.path.join(top, f)
+            mode = os.stat(pathname).st_mode
+            if S_ISDIR(mode):
+                self.create_directory(top, pathname)
+                self.process_directory(pathname)
+            elif S_ISREG(mode):
+                self.upload_file(top, pathname)
 
-def create_directory(basedir, top, pathname):
-    if top[-1] != os.path.sep:
-        top = top + os.path.sep
-    baseprefix = len(os.path.commonprefix([basedir, top]))
-    base = pathname[baseprefix:]
-    path = "%s%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, base)
-    req = urllib2.Request(path, '')
-    req.add_header("Content-type", "application/directory")
-    req.add_header("X-Auth-Token", get_auth_token())
-    req.get_method = lambda: "PUT"
-    res = urllib2.urlopen(req).read()
-    if res or res == "":
-        print "Created directory: %s" % base
-    else:
-        print res
+    def create_directory(self, top, pathname):
+        if top[-1] != os.path.sep:
+            top = top + os.path.sep
+        baseprefix = len(os.path.commonprefix([self.basedir, top]))
+        base = pathname[baseprefix:]
+        path = "%s%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, base)
+        req = urllib2.Request(path, '')
+        req.add_header("Content-Type", "application/directory")
+        req.add_header("X-Auth-Token", get_auth_token())
+        req.get_method = lambda: "PUT"
+        res = urllib2.urlopen(req).read()
+        if res or res == "":
+            print "Created directory: %s" % base
+        else:
+            print res
 
-def upload_file(basedir, top, pathname):
-    if top[-1] != os.path.sep:
-        top = top + os.path.sep
-    baseprefix = len(os.path.commonprefix([basedir, top]))
-    base = pathname[baseprefix:]
-    path = "%s%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, base)
-    req = urllib2.Request(path, open(pathname).read())
-    req.add_header("X-Auth-Token", get_auth_token())
-    req.get_method = lambda: "PUT"
-    if urllib2.urlopen(req).read() == "":
-        print "Uploaded: %s" % pathname
+    def upload_file(self, top, pathname):
+        if top[-1] != os.path.sep:
+            top = top + os.path.sep
+        baseprefix = len(os.path.commonprefix([self.basedir, top]))
+        base = pathname[baseprefix:]
+        path = "%s%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, base)
+        req = urllib2.Request(path, open(pathname).read())
+        ext = os.path.splitext(path)[1]
+        mimetype = mimetypes.types_map.get(ext)
+        if mimetype:
+            req.add_header("Content-Type", mimetype)
+
+        req.add_header("X-Auth-Token", get_auth_token())
+        req.get_method = lambda: "PUT"
+        if urllib2.urlopen(req).read() == "":
+            print "Uploaded: %s" % pathname
