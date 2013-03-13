39513d7f5d4eb4e9953d030af56bb7cf88c2613c
AeroNotix <aaron.l.france@gmail.com>
2013-03-13 13:30:56 +0100

    Setup.py additions.

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from distutils.core import setup
 setup(name='django_hpcloud',
       version='0.1',
       description='HPCloud module for Django',
+      long_description="""
+Django Module for interacting with the HPCloud services.
+""",
       author='Aaron France',
       author_email='aaron.france@hp.com',
       url='http://github.com/AeroNotix/django-hpcloud',
       package_dir={
         'django_hpcloud': ''
         },
+      license="BSD",
+      platform=["windows", "linux", "mac"],
       packages=[
         'django_hpcloud',
         'django_hpcloud.templatetags',
