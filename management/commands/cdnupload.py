9a6ea25127610e72be1a378a8f901b1036a625ff
AeroNotix <aaron.l.france@gmail.com>
2013-03-13 17:01:08 +0100

    cdnupload now takes a string for the name of the base container to put files into, this is so files which reference other containers are able to access each other correctly.

--- management/commands/cdnupload.py
+++ management/commands/cdnupload.py
@@ -16,43 +16,61 @@ class Command(BaseCommand):
             self.settings = settings.HPCLOUD_TTL_MAP
         except AttributeError:
             self.settings = {}
-
+        if not args:
+            self.stdout.write("A container name is required.\n")
+            return
+        self.container = args[0]
+        # will create the base container
+        self.create_directory(self.prepare_path(""))
         for directory in settings.STATICFILES_DIRS:
             self.basedir = directory
             self.process_directory(directory)
 
     def process_directory(self, top):
+        '''
+        process_directory will iterate recursively through an entire directory
+        and create a container on the ObjectStore for each directory and upload
+        files to the ObjectStore for each file.
+        '''
         for f in os.listdir(top):
             pathname = os.path.join(top, f)
             mode = os.stat(pathname).st_mode
             if S_ISDIR(mode):
-                self.create_directory(top, pathname)
+                self.create_subdirectory(top, pathname)
                 self.process_directory(pathname)
             elif S_ISREG(mode):
                 self.upload_file(top, pathname)
 
-    def create_directory(self, top, pathname):
-        if top[-1] != os.path.sep:
-            top = top + os.path.sep
-        baseprefix = len(os.path.commonprefix([self.basedir, top]))
-        base = pathname[baseprefix:]
-        path = "%s%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, base)
-        req = urllib2.Request(path, '')
+    def create_directory(self, pathname):
+        req = urllib2.Request(pathname, '')
         req.add_header("Content-Type", "application/directory")
         req.add_header("X-Auth-Token", get_auth_token())
         req.get_method = lambda: "PUT"
         res = urllib2.urlopen(req).read()
         if res or res == "":
-            print "Created directory: %s" % base
+            self.stdout.write("Created directory: %s\n" % pathname)
         else:
-            print res
+            self.stdout.write("%s\n" % res)
+
+    def create_subdirectory(self, top, pathname):
+        '''
+        create_directory will perform a PUT on the ObjectStore to create a directory
+        with application/directory Content-type so that it will be created as
+        an ObjectStore container.
+        '''
+        if top[-1] != os.path.sep:
+            top = top + os.path.sep
+        baseprefix = len(os.path.commonprefix([self.basedir, top]))
+        base = pathname[baseprefix:]
+        path = self.prepare_path(base)
+        return self.create_directory(path)
 
     def upload_file(self, top, pathname):
         if top[-1] != os.path.sep:
             top = top + os.path.sep
         baseprefix = len(os.path.commonprefix([self.basedir, top]))
         base = pathname[baseprefix:]
-        path = "%s%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, base)
+        path = self.prepare_path(base)
         req = urllib2.Request(path, open(pathname).read())
         ext = os.path.splitext(path)[1]
         mimetype = mimetypes.types_map.get(ext)
@@ -64,3 +82,6 @@ class Command(BaseCommand):
         req.get_method = lambda: "PUT"
         if urllib2.urlopen(req).read() == "":
             print "Uploaded: %s" % pathname
+
+    def prepare_path(self, path):
+        return "%s%s/%s/%s" % (settings.OBJECT_STORE_URL, settings.TENANT_ID, self.container, path)
